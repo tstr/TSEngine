9cb45ad49d0e2c1ff130f9228e94ea69a0f11013
BrotherhoodOfHam <thomas.street@florimont.com>
2018-04-22 20:57:42 +0100

    Fixed error with finding the wrong python libraries.

--- cmake/PythonUtils.cmake
+++ cmake/PythonUtils.cmake
@@ -17,7 +17,9 @@ macro(add_pybind_module _target)
     add_library(${_target} SHARED ${ARGN})
     set_target_properties(${_target} PROPERTIES PREFIX "${PYTHON_MODULE_PREFIX}")
     set_target_properties(${_target} PROPERTIES SUFFIX "${PYTHON_MODULE_EXTENSION}")
-    target_link_libraries(${_target} PRIVATE pybind11)
+    target_link_libraries(${_target} PRIVATE pybind11)	
+	target_link_libraries(${_target} PRIVATE ${PYTHON_LIBRARIES})
+	target_include_directories(${_target} PRIVATE "${PYTHON_INCLUDE_DIR}")
 endmacro()
 
 
@@ -31,7 +33,19 @@ endif()
 # Environment wrapper, configures PYTHONPATH:
 #   - adds runtime output dir
 #   - adds project root
-set(PYWRAPPER_COMMAND
-    "${CMAKE_COMMAND}" -E env "PYTHONPATH=${PROJECT_BINARY_DIR}/bin/$<CONFIG>/${_sep}${PROJECT_SOURCE_DIR}${_sep}${PROJECT_SOURCE_DIR}/tools/${_sep}$ENV{PYTHONPATH}"
-    "${PYTHON_EXECUTABLE}"
+function(format_pypath_cmd _output_var)
+    set(_module_paths)
+    foreach(_path ${ARGN})
+        set(_module_paths "${_module_paths}${_sep}${_path}")
+    endforeach()
+    
+    set(${_output_var} "${CMAKE_COMMAND}" -E env "PYTHONPATH=${_module_paths}$ENV{PYTHONPATH}" "${PYTHON_EXECUTABLE}" PARENT_SCOPE)
+endfunction()
+
+# Configure python path for tool scripts
+format_pypath_cmd(
+    PYWRAPPER_COMMAND
+    "${PROJECT_BINARY_DIR}/bin/$<CONFIG>/"
+    "${PROJECT_SOURCE_DIR}"
+    "${PROJECT_SOURCE_DIR}/tools/"
 )
